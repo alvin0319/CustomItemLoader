3fae57d2be05931ed03c35e3e30a02aef8eba201
alvin0319 <alvin7810@naver.com>
2020-12-22 13:31:35 +0900

    CustomDurableItem: fixed item being not damaged

--- src/alvin0319/CustomItemLoader/item/CustomDurableItem.php
+++ src/alvin0319/CustomItemLoader/item/CustomDurableItem.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace alvin0319\CustomItemLoader\item;
 
+use pocketmine\block\Block;
+use pocketmine\entity\Entity;
 use pocketmine\item\Durable;
 
 class CustomDurableItem extends Durable{
@@ -24,5 +26,13 @@ class CustomDurableItem extends Durable{
 
 	public function getMaxStackSize() : int{
 		return $this->maxStackSize;
+	}
+
+	public function onDestroyBlock(Block $block) : bool{
+		return $this->applyDamage(1);
+	}
+
+	public function onAttackEntity(Entity $victim) : bool{
+		return $this->applyDamage(1);
 	}
 }
