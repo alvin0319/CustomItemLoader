9442bafb661cfa249515e2f483be946987a7c507
alvin0319 <alvin7810@naver.com>
2023-01-26 19:30:19 +0900

    Do not refresh everytime, just once

--- src/alvin0319/CustomItemLoader/CustomItemManager.php
+++ src/alvin0319/CustomItemLoader/CustomItemManager.php
@@ -124,7 +124,6 @@ final class CustomItemManager{
 		}catch(Throwable $e){
 			throw new \InvalidArgumentException("Failed to register item: " . $e->getMessage(), $e->getLine(), $e);
 		}
-		$this->refresh();
 	}
 
 	private function refresh() : void{
@@ -146,6 +145,7 @@ final class CustomItemManager{
 		foreach($data as $name => $itemData){
 			$this->registerItem(self::getItem($name, $itemData));
 		}
+		$this->refresh();
 	}
 
 	public static function getItem(string $name, array $data) : Item{
